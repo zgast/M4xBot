d5bf3cc39dc408723e1c81a39c5344ce95f83013
zgast <mrks@zgast.at>
2021-02-16 10:27:47 +0100

    size check

--- src/at/markus/Main.java
+++ src/at/markus/Main.java
@@ -7,7 +7,7 @@ import javax.security.auth.login.LoginException;
 public class Main {
 
     public static void main(String[] args) {
-        JDABuilder jdaBuilder = JDABuilder.createDefault("ODEwOTU0NjU3MjUzOTQ5NDYx.YCrKYQ.PglxChGQSmyptkcFVaQc1RjlufE").addEventListeners(new Listener());
+        JDABuilder jdaBuilder = JDABuilder.createDefault("").addEventListeners(new Listener());
         try {
             jdaBuilder.build();
         } catch (LoginException e) {
--- src/at/markus/URLChecker.java
+++ src/at/markus/URLChecker.java
@@ -1,19 +1,39 @@
 package at.markus;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
 public class URLChecker {
     public boolean checkURL(String message){
         URLConnection connection = null;
-        try {
-            connection = new URL(message).openConnection();
-            String contentType = connection.getHeaderField("Content-Type");
-            return contentType.startsWith("image/");
-        } catch (IOException e) {
-            e.printStackTrace();
+        if(getFileSize(message)<30000000){
+            try {
+                connection = new URL(message).openConnection();
+                String contentType = connection.getHeaderField("Content-Type");
+                return contentType.startsWith("image/");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+
         return false;
+    }
+
+    public long getFileSize(String url) {
+        HttpURLConnection conn = null;
+        try {
+            conn = (HttpURLConnection) new URL(url).openConnection();
+            conn.setRequestMethod("HEAD");
+            return conn.getContentLengthLong();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
     }
 }
